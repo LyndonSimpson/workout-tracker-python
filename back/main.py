5b9c181f1b7a3bbc2eeefdc4ae4d1bd74fe04152
NovaBison <novabison92@gmail.com>
2026-02-15 11:19:59 +0100

    added pseudocode

--- back/main.py
+++ back/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# import routes here
+#create a controller, a datamapper, and then map all 
+# routes in a routerm then call therouters here, then 
+# crate middlewares like jwt etc - and protect routes in router for example. just like we learned to do an old API
+
 @app.get("/")
 async def root():
     return {"message": "Raccoons rule"}
