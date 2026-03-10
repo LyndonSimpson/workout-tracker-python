a2aa83d4ec91f260cd3a82f9e11df91ee98562b0
NovaBison <novabison92@gmail.com>
2026-03-10 19:34:03 +0100

    adding get workout route

--- prototype/router/workout_router.py
+++ prototype/router/workout_router.py
@@ -3,6 +3,7 @@ from uuid import UUID
 
 from eventsourcing.application import AggregateNotFoundError
 from fastapi import APIRouter, Depends, HTTPException, Query, status
+from schema.workout import WorkoutResponse
 
 router = APIRouter(prefix="/workouts", tags=["Basic workout tracker for reps and max weights progress"])
 
@@ -10,3 +11,15 @@ router = APIRouter(prefix="/workouts", tags=["Basic workout tracker for reps and
 @router.post("/sanity_check")
 def sanity():
     return {"message": "Sanity check ok"}
+
+## need to create the controller now:
+router.get("/{workout_id}", response_model=WorkoutResponse)
+def get_workout(
+    workout_id: UUID,
+    controller: WorkoutController = Depends(get_workout_controller),
+) -> WorkoutResponse:
+    try:
+        data = controller.get_workout(workout_id)
+        return WorkoutResponse(**data)
+    except AggregateNotFoundError as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Workout not found") from exc
